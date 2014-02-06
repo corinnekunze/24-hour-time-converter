e0844ebc9557e81ef2da79f6bd12bfabe6a9efc2
corinnekunze <corinnekunze@gmail.com>
2014-02-06 08:48:04 -0800

    Added if statement to start converting from 12 to 24

--- convert.js
+++ convert.js
@@ -4,23 +4,38 @@
 //	alert(firstTime.match(re));
 // }
 
-function ConvertTimes() {
-	var initialTime = document.getElementById("first-time").value;
-	var parts = initialTime.split(':'),
-	hour = parts[0],
-	minutes = parts[1];
-
-	if (hour > 12) {
-        initialTime = (hour - 12) + ':' + minutes + ' pm';
-    } else if (hour == 0) {
-        initialTime = 12 + ':' + minutes + ' am';
-    } else if (hour == 12) {
-        initialTime += ' pm';
-    } else {
-        initialTime += ' am';
+function activeInput(x) {
+    if (x === 'military') {
+        document.getElementById('first-time').classList.remove('firstLoad');
+        document.getElementById('second-time').classList.remove('active');
+        document.getElementById('first-time').classList.add('active');
     }
-    var convertedTime = initialTime;
-    document.getElementById("second-time").value = convertedTime;
+    else if (x === '12hour') {
+        document.getElementById('first-time').classList.remove('active');
+        document.getElementById('second-time').classList.add('active');
+    }
+}
+
+function ConvertTimes() {
+    if (document.getElementById('first-time').classList.contains('active') || document.getElementById('first-time').classList.contains('firstLoad')) {
+
+    	var initialTime = document.getElementById("first-time").value;
+    	var parts = initialTime.split(':'),
+    	hour = parts[0],
+    	minutes = parts[1];
+
+    	if (hour > 12) {
+            initialTime = (hour - 12) + ':' + minutes + ' pm';
+        } else if (hour == 0) {
+            initialTime = 12 + ':' + minutes + ' am';
+        } else if (hour == 12) {
+            initialTime += ' pm';
+        } else {
+            initialTime += ' am';
+        }
+        var convertedTime = initialTime;
+        document.getElementById("second-time").value = convertedTime;
+    }
 }
 
 
