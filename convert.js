4e66bebf6a69cdd51081f4355dc8af19ab3f0238
corinnekunze <corinnekunze@gmail.com>
2014-02-06 21:51:21 -0800

    Added backward compatability 12 to 24 hour

--- convert.js
+++ convert.js
@@ -4,37 +4,52 @@
 //	alert(firstTime.match(re));
 // }
 
+function byId(id) {
+    return document.getElementById(id);
+}
+
 function activeInput(x) {
     if (x === 'military') {
-        document.getElementById('first-time').classList.remove('firstLoad');
-        document.getElementById('second-time').classList.remove('active');
-        document.getElementById('first-time').classList.add('active');
+        byId('second-time').classList.remove('active');
+        byId('first-time').classList.add('active');
     }
     else if (x === '12hour') {
-        document.getElementById('first-time').classList.remove('active');
-        document.getElementById('second-time').classList.add('active');
+        byId('first-time').classList.remove('active');
+        byId('second-time').classList.add('active');
     }
 }
 
 function ConvertTimes() {
-    if (document.getElementById('first-time').classList.contains('active') || document.getElementById('first-time').classList.contains('firstLoad')) {
-
-    	var initialTime = document.getElementById("first-time").value;
-    	var parts = initialTime.split(':'),
+    if (byId('first-time').classList.contains('active') || byId('first-time').classList.contains('firstLoad')) {
+    	var militaryTime = byId("first-time").value;
+    	var parts = militaryTime.split(':'),
     	hour = parts[0],
     	minutes = parts[1];
 
     	if (hour > 12) {
-            initialTime = (hour - 12) + ':' + minutes + ' pm';
+            militaryTime = (hour - 12) + ':' + minutes + ' pm';
         } else if (hour == 0) {
-            initialTime = 12 + ':' + minutes + ' am';
+            militaryTime = 12 + ':' + minutes + ' am';
         } else if (hour == 12) {
-            initialTime += ' pm';
+            militaryTime += ' pm';
         } else {
-            initialTime += ' am';
+            militaryTime += ' am';
         }
-        var convertedTime = initialTime;
-        document.getElementById("second-time").value = convertedTime;
+        var convertedTime = militaryTime;
+        byId("second-time").value = convertedTime;
+    } else if (byId('second-time').classList.contains('active')) {
+        var twelveHour = byId("second-time").value;
+        var hours = Number(twelveHour.match(/^(\d+)/)[1]);
+        var minutes = Number(twelveHour.match(/:(\d+)/)[1]);
+        var AMPM = twelveHour.match(/\s(.*)$/)[1];
+        if (AMPM == "PM" || "pm" && hours < 12) hours = hours + 12;
+        if (AMPM == "AM" || "am" && hours == 12) hours = hours - 12;
+        var sHours = hours.toString();
+        var sMinutes = minutes.toString();
+        if (hours < 10) sHours = "0" + sHours;
+        if (minutes < 10) sMinutes = "0" + sMinutes;
+        var endTime = sHours + ":" + sMinutes;
+        byId("first-time").value = endTime;
     }
 }
 
